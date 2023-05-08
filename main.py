974bd6d312841373ca5e6ab694e3cead2004cd96
MarlinZH <Marlin.z.hicks@gmail.com>
2023-05-08 11:49:09 -0400

    Creation of a project

---
+++ main.py
@@ -0,0 +1,15 @@
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import osmnx
+import shapely
+import pandas as pd
+import numpy as np
+import networkx as nx
+
+def point_finder(place,tags):
+    '''
+    Returns a dataframe of coordinates of an entity from OSM.
+    :param place(str): A location
+    :param tags(dict):key value of entity attribute in OSM and value
+    :return: results(DataFrame): table of latitude and longitude with entity value
+    '''
