ca5dc0b39e46eb132c1186239e50d5e99ce4722f
MarlinZH <marlin.z.hicks@gmail.com>
2023-05-08 16:20:37 -0400

    Creation of a project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
-from shapely.geometry import Point, Polygon
+from shapely.geometry import point, polygon
 import osmnx
 import shapely
 import pandas as pd
@@ -14,16 +14,28 @@ def point_finder(place,tags):
     :return: results(DataFrame): table of latitude and longitude with entity value
     '''
     gdf = osmnx.geocode_to_gdf(place)
+    print(gdf)
     bounding = gdf.bounds
-    north, south, east , west = bounding.iloc[0,3], bounding.iloc[0,1], bounding.iloc[0,2], bounding.iloc[0,0]
-    location = gdf.geometry.unarty_union
+    print(bounding)
+    north, south, east, west = bounding.iloc[0, 3], bounding.iloc[0, 1], bounding.iloc[0, 2], bounding.iloc[0, 0]
 
-    point = osmnx.geometries_from_bbox(north,
-                                       south,
-                                       east,
-                                       west,
-                                       tags = tags)
-    points.set_crs(crs=4326)
-    point = point[point.geometry.within(location)]
+    # location = gdf.unary_union
+    # print(location)
+    p = {'name':[place],'geometry':[north,south,east,west]}
+    point = gpd.GeoDataFrame(p,crs="EPSG:4326")
+    # tags = tags)
+    print('THIS IS THE POINT')
+    # point.set_crs(4326)
+    print(point.crs)
+    # point = point[point.geometry.within(location)]
 
-    point['geometry'] = point[geometry].apply(lambda x:x.centroid if type(x) == Polygon else x)
+    # point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
+    # point = point[point.geom_type != 'MultiPolygon' ]
+    # point = point[point.geom_type != 'Polygon']
+
+    results = pd.dataframe({'name':list(point['name']),
+                            'longitude':list(point['geometry'].x),
+                            'latitude':list(point['geometry'].y)})
+    results['name'] = list(tags.values())[0]
+    return results
+convenience_stores = point_finder(place= 'Shinjuku,Tokyo',tags={"brand:en":""})
