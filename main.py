401ee1023762070f392f91433a17056f494b6bac
MarlinZH <marlin.z.hicks@gmail.com>
2023-05-10 00:30:27 -0400

    Modifications of GEODATAFRAME CREATION

--- main.py
+++ main.py
@@ -19,24 +19,25 @@ def point_finder(place,tags):
     print('BOUNDING:',bounding)
     north, south, east, west = bounding.iloc[0, 3], bounding.iloc[0, 1], bounding.iloc[0, 2], bounding.iloc[0, 0]
     print('North:{},South:{},East:{},West{}'.format(north,south,east,west))
-    p = {'name':[place],'geometry':[north,south,east,west]}
-    point = osmnx.geometries_from_bbox(north,south,east, west, tags)
-    #point = gpd.GeoDataFrame(p['name'],geometry=p['geometry'])
-    # tags = tags)
-    print('THIS IS THE POINT')
+    p = gpd.GeoDataFrame({'name':['North','South','East','West'],'geometry':[north,south,east,west]})
+
+    # point = osmnx.geometries_from_bbox(north,south,east, west, tags)
+    # #point = gpd.GeoDataFrame(p['name'],geometry=p['geometry'])
+    # # tags = tags)
+    # print('THIS IS THE POINT',point)
     # point.set_crs(4326)
-    print(point.crs)
-    location = gdf.unary_union
-    print(location)
-    # point = point[point.geometry.within(location)]
+    # print(point.crs)
+    # location = gdf.unary_union
+    #print(location)
+    # # point = point[point.geometry.within(location)]
+    #
+    # # point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
+    # # point = point[point.geom_type != 'MultiPolygon' ]
+    # # point = point[point.geom_type != 'Polygon']
 
-    # point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
-    # point = point[point.geom_type != 'MultiPolygon' ]
-    # point = point[point.geom_type != 'Polygon']
-
-    results = pd.dataframe({'name':list(point['name']),
-                            'longitude':list(point['geometry'].x),
-                            'latitude':list(point['geometry'].y)})
+    results = pd.DataFrame({'name':list(p['name']),
+                            'longitude':list(p['geometry']),
+                            'latitude':list(p['geometry'])})
     results['name'] = list(tags.values())[0]
     return results
 convenience_stores = point_finder(place= 'Shinjuku,Tokyo',tags={"brand:en":""})
