4cbd04d93c08526ffc3f01dae3bdc0ef4ebb9805
MarlinZH <marlin.z.hicks@gmail.com>
2023-05-09 23:45:59 -0400

    Modifications of GEODATAFRAME CREATION

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def point_finder(place,tags):
     print('BOUNDING:',bounding)
     north, south, east, west = bounding.iloc[0, 3], bounding.iloc[0, 1], bounding.iloc[0, 2], bounding.iloc[0, 0]
     print('North:{},South:{},East:{},West{}'.format(north,south,east,west))
-    location = gdf.unary_union
-    print(location)
     p = {'name':[place],'geometry':[north,south,east,west]}
     point = osmnx.geometries_from_bbox(north,south,east, west, tags)
     #point = gpd.GeoDataFrame(p['name'],geometry=p['geometry'])
@@ -28,6 +26,8 @@ def point_finder(place,tags):
     print('THIS IS THE POINT')
     # point.set_crs(4326)
     print(point.crs)
+    location = gdf.unary_union
+    print(location)
     # point = point[point.geometry.within(location)]
 
     # point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
