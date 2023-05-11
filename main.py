41626fc58f21886470474b66435f271bc8f05038
MarlinZH <marlin.z.hicks@gmail.com>
2023-05-11 15:54:54 -0400

    CREATING A CLASS TO PULL ALL ENTITIES IN A SPECIFIC AREA BY TAGS

--- main.py
+++ main.py
@@ -6,41 +6,79 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 
-def point_finder(place,tags):
+def area_entities(area,tags):
+    '''
+    Returns a dataframe of entities in an area grouped by Brand and the quantities of each brand
+    :param area:
+    :param tags:
+    :return:
+    '''
+    gdf = osmnx.geocode_to_gdf(area)
+    print(gdf)
+    # Gets the bounding box of the gdf GeoDataFrame
+    bounding = gdf.bounds
+    print('BOUNDING:', bounding)
+    north, south, east, west = bounding.iloc[0, 3], bounding.iloc[0, 1], bounding.iloc[0, 2], bounding.iloc[0, 0]
+    print('North: {},South: {},East: {},West: {}'.format(north, south, east, west))
+    location = gdf.unary_union
+    # Find Points within the polygon
+    point = osmnx.geometries_from_bbox(north, south, east, west, tags)
+    print('Point:', point)
+    point.set_crs(4326)
+    print(point.crs)
+    # location = gdf.unary_union
+    # print(location)
+    point = point[point.geometry.within(location)]
+    point['geometry'] = point['geometry'].apply(lambda x: x.centroid if type(x) == polygon else x)
+    point = point[point.geom_type != 'MultiPolygon']
+    point = point[point.geom_type != 'Polygon']
+
+    results = pd.DataFrame({'name': list(point['name']),
+                            'brand': list(point['brand:en']),
+                            'longitude': list(point['geometry'].x),
+                            'latitude': list(point['geometry'].y)})
+    results['name'] = list(tags.values())[0]
+    print(results)
+    return results
+
+def point_finder(area,tags):
     '''
     Returns a dataframe of coordinates of an entity from OSM.
-    :param place(str): A location
-    :param tags(dict):key value of entity attribute in OSM and value
+    :param area(str): A location
+    :param tags(dict): key value of entity attribute in OSM and value
     :return: results(DataFrame): table of latitude and longitude with entity value
     '''
     #Creates GeoDataframe of longitude and Latitude of place
-    gdf = osmnx.geocode_to_gdf(place)
+    gdf = osmnx.geocode_to_gdf(area)
     print('GDF:',gdf)
     #Gets the bounding box of the gdf GeoDataFrame
     bounding = gdf.bounds
     print('BOUNDING:',bounding)
     north, south, east, west = bounding.iloc[0, 3], bounding.iloc[0, 1], bounding.iloc[0, 2], bounding.iloc[0, 0]
     print('North: {},South: {},East: {},West: {}'.format(north,south,east,west))
-    #Find Points within the are polygon
-    p = gpd.GeoDataFrame({'name':['North','South','East','West'],'geometry':[north,south,east,west]})
-    print(p)
-    # point = osmnx.geometries_from_bbox(north,south,east, west, tags)
+    location = gdf.unary_union
+    #Find Points within the polygon
+    point = osmnx.geometries_from_bbox(north, south, east, west,tags)
+    print('Point:',point)
+    # p = gpd.GeoDataFrame({'name':['North','South','East','West'],'geometry':[north,south,east,west]})
+    # print('P:',p)
+
     # #point = gpd.GeoDataFrame(p['name'],geometry=p['geometry'])
     # # tags = tags)
     # print('THIS IS THE POINT',point)
-    # point.set_crs(4326)
-    # print(point.crs)
+    point.set_crs(4326)
+    print(point.crs)
     # location = gdf.unary_union
     #print(location)
-    # # point = point[point.geometry.within(location)]
-    #
-    # # point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
-    # # point = point[point.geom_type != 'MultiPolygon' ]
-    # # point = point[point.geom_type != 'Polygon']
+    point = point[point.geometry.within(location)]
+    point['geometry'] = point['geometry'].apply(lambda x:x.centroid if type(x) == polygon else x)
+    point = point[point.geom_type != 'MultiPolygon' ]
+    point = point[point.geom_type != 'Polygon']
 
-    results = pd.DataFrame({'name':list(p['name']),
-                            'longitude':list(p['geometry']),
-                            'latitude':list(p['geometry'])})
+    results = pd.DataFrame({'name':list(point['name']),
+                            'longitude':list(point['geometry'].x),
+                            'latitude':list(point['geometry'].y)})
     results['name'] = list(tags.values())[0]
+    print(results)
     return results
-convenience_stores = point_finder(place= 'Shinjuku,Tokyo',tags={"brand:en":"711"})
+convenience_stores = area_entities(area= 'Shinjuku,Tokyo',tags={"shop":"convenience"})
