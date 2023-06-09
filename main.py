0cb59a31f13362f4f5a71332252772f06a1ce85e
Marlin <74386482+MarlinZH@users.noreply.github.com>
2023-06-08 22:15:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ df['indices']=[[val for val in row if val != idx] for idx,row in enumerate(df['i
 #Create temporary index column
 entities_list = entities_list.reset_index()
 #Set Temporary Index Colimn as Index
-----
+-
